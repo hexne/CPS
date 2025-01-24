a470d1f6a47d24ad4d1fea7034b5bc857ecea170
hexne <printfne@gmail.com>
2025-01-25 00:29:08 +0800

    对参数进行解析

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
-#include <QCoreApplication>
-#include <QDebug>
+import Param;
 
+bool enable_gui;
 int main(int argc, char* argv[]) {
-    QCoreApplication a(argc, argv);
-    qDebug() << "Hello World";
-    return QCoreApplication::exec();
+
+    nl::Param param(argc, argv);
+    param.add_param("g", "gui", [&] {
+        enable_gui = true;
+    });
+    param.analyze();
+
+    // 开启GUI支持
+    if (enable_gui) {
+
+    }
+    // 仅为命令行模式
+    else {
+        
+    }
+
+    return 0;
 }
